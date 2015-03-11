df5eae8b11039856ba1435aaa623cfbf4c3212ad
admin <admin@10.8.101.132>
2015-03-11 15:28:28 +0700

    tag a href

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,4 +1,36 @@
 <?php
+use yii\helpers\Html;
+
 /* @var $this yii\web\View */
 $this->title = 'กรมอนามัย';
 ?>
+
+<?php 
+   
+ $route1=  Yii::$app->urlManager->createUrl('test/test1')
+ ?>
+<a href="<?=$route1?>">ไปที่ test1</a>
+<br>
+
+<?php 
+   
+ $route2=  Yii::$app->urlManager->createUrl(['test/test2','name'=>'pakorn chuaysakun'])
+ ?>
+<a href="<?=$route2?>">ไปที่ test2</a>
+<br>
+
+<?php 
+    
+ echo Html::a('ลิงค์แบบที่ 3', ['test/test1']);
+?>
+<br>
+<?php 
+    
+ echo Html::a('ลิงค์แบบที่ 4', ['test/test2','name'=>'jameshpc11']);
+?>
+
+<?php 
+    echo yii\helpers\Url::to(['test/test2','name'=>'jameshpc11'])
+?>
+
+
