bac1780acab793ae72af0a829ba3bf010673b21c
admin <admin@10.8.102.181>
2015-03-12 15:09:42 +0700

    kartik

--- frontend/views/report/report1.php
+++ frontend/views/report/report1.php
@@ -9,8 +9,32 @@ echo GridView::widget([
     'dataProvider' => $dataProvider,
     //'filterModel' => $searchModel,
     'panel' => [
-        'before' => 'รายงาน',
+        'before' => 'รายงานนับถือศาสนา',
         'after' => 'ประมวลผล ณ ' . date('Y-m-d H:i:s')
-    ]
+    ],
+    'columns'=>[
+        ['class'=>'yii\grid\SerialColumn'],
+        
+        ['attribute'=>'hoscode',
+        'label'=>'รหัสสถานบริการ',
+        ],
+        
+        ['attribute'=>'hosname',
+        'label'=>'หน่วยบริการ',
+        ],
+        
+         ['attribute'=>'total',
+        'label'=>'ประชากรทั้งหมด(คน)',
+        ],
+        
+        ['attribute'=>'buddha',
+        'label'=>'ศาสนาพุทธ (คน)',
+        ],
+        
+        ['attribute'=>'other',
+        'label'=>'ศาสนาพุทธ (คน)',
+        ],
+    ],
+    
 ])
 ?>
